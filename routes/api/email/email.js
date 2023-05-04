009065e6d6ffc4c65fad9f35ec909b102d477559
dcarrasco <dcarrascocid@gmail.com>
2023-05-03 22:28:15 -0400

    mandando correo

---
+++ routes/api/email/email.js
@@ -0,0 +1,39 @@
+import { Router } from 'express' 
+
+import HelperEmail  from './../../../lib/helpers/email_helper'
+
+ 
+
+class Email{
+
+    constructor()
+    {
+        const api = Router()
+
+        api.post('/send', this.send)
+        
+        return api
+    }
+
+    async send(req, res) 
+    {
+
+
+        const cm = new HelperEmail()  
+
+        try {
+            await cm.sendQuotation({correo: 'dpailahueque01@gmail.com'})   
+            console.log('enviado')
+            res.status(200).json({ok: true})
+            
+        } catch (error) {
+            console.error('Error: ', error)
+            res.status(200).json({ok: false})
+        }
+
+        console.log('paso todo')
+
+    } 
+
+}
+export default Email;
